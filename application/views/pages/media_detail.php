1ffb93bf5b8f9c90ed3be7ceec6fd46c6dffcdc5
tapanbavaliya <tbavaliya@gmail.com>
2014-10-11 23:59:06 +0530

    done issue of Implement proper approach for media to show in small screen

--- application/views/pages/media_detail.php
+++ application/views/pages/media_detail.php
@@ -16,7 +16,8 @@
         <div id="media-gallary-image" class="col-md-6 col-sm-4 col-xs-12 gallary images">
           <!-- {{media_content}} -->
           <?php foreach ($images as $key => $value) { ?>
-          <div class="img-thumb"><img src=<?='"'.$value->link.'"'?> title=<?='"'.$value->name.'"'?> ></div>
+          <div class="img-thumb hidden-xs"><img src=<?='"'.$value->link.'"'?> title=<?='"'.$value->name.'"'?> ></div>
+          <div class="img-thumb visible-xs" data-toggle="modal" data-target="#myModal"><img src=<?='"'.$value->link.'"'?> title=<?='"'.$value->name.'"'?> ></div>
           <?php } ?>
         </div>
         <div id="media-gallary-video" class="col-md-6 col-sm-4 col-xs-12 gallary video" style="display: none">
@@ -27,7 +28,7 @@
           </div>
           <?php } ?>
         </div>
-        <div class="col-md-6 col-sm-8 col-xs-12">
+        <div class="col-md-6 col-sm-8 col-xs-12 hidden-xs">
           <div class="img-thumb-p text-center"><img src=""></div>
         </div>
       </div>
@@ -38,6 +39,22 @@
       </div>
     </div>
   </div>
+  
+<div class="visible-xs">
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <div class="img-thumb-p text-center" style="height:100%"><img src=""></div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 </div>
 </div>
 </div>
