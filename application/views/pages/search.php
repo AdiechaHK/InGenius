2b11d1c33e0bd9d76de4926b0c5b61a27b6f99fb
tapan <tbavaliya@gmail.com>
2014-10-12 23:56:49 +0530

    done search page

--- application/views/pages/search.php
+++ application/views/pages/search.php
@@ -1,4 +1,68 @@
-<div class="container">
-	<h1>Search result</h1>
+<div class="clearfix">
+        
+  <div class="pos-img-vid clearfix">
+    <div class="img-vid">
+      <strong>Search Result</strong>
+    </div>
+    <img src="http://localhost:8080/InGenius/public/img/bottom-img.png">
+  </div>
+  
+  <br>
+
+  <div class="row border-class">
+    
+    <div class="col-xs-12">
+
+      <div class="margin-top-bottom">
+        <div class="clearfix">
+          <a href="" class="search-link-style">
+          <div class="title-c4 color-red">
+            What really makes for a good idea?
+          </div>
+          </a>
+        </div>
+        <div class="clearfix">
+          <div class="title-c41">
+            It is not argued that the socio-political and economic conditions in present day Nigeria are not the best they could be. Nevertheless, we cannot deny the fact that there is still some progress being made in many areas.
+          </div>
+        </div>
+      </div>
+
+      <div class="margin-top-bottom">
+        <div class="clearfix">
+          <a href="" class="search-link-style">
+          <div class="title-c4 color-red">
+            What really makes for a good idea?
+          </div>
+          </a>
+        </div>
+        <div class="clearfix">
+          <div class="title-c41">
+            It is not argued that the socio-political and economic conditions in present day Nigeria are not the best they could be. Nevertheless, we cannot deny the fact that there is still some progress being made in many areas.
+          </div>
+        </div>
+      </div>
+
+
+      <div class="margin-top-bottom">
+        <div class="clearfix">
+          <a href="" class="search-link-style">
+          <div class="title-c4 color-red">
+            What really makes for a good idea?
+          </div>
+          </a>
+        </div>
+        <div class="clearfix">
+          <div class="title-c41">
+            It is not argued that the socio-political and economic conditions in present day Nigeria are not the best they could be. Nevertheless, we cannot deny the fact that there is still some progress being made in many areas.
+          </div>
+        </div>
+      </div>
+
+    </div>
+  
+  </div>
+         
 </div>
+
 </div>
